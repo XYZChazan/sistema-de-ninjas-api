f736743a57c4de22bccc91abfe84661489abfbcc
Vitor Oliveira <chazan.uni@gmail.com>
2025-03-11 09:10:42 -0300

    first commit

---
+++ src/main/java/com/chazan/sistema_de_ninjas_api/SistemaDeNinjasApiApplication.java
@@ -0,0 +1,13 @@
+package com.chazan.sistema_de_ninjas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeNinjasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeNinjasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chazan/sistema_de_ninjas_api/SistemaDeNinjasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chazan.sistema_de_ninjas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeNinjasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
