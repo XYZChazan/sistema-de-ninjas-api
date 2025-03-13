6e506f80518004c4a13d1f466cad31c56c96c87d
Vitor Oliveira <chazan.uni@gmail.com>
2025-03-13 13:04:30 -0300

    Ajuste do nome da controller e implementando a entidade NinjaEntity

---
+++ src/main/java/com/chazan/sistema_de_ninjas_api/NinjaEntity.java
@@ -0,0 +1,52 @@
+package com.chazan.sistema_de_ninjas_api;
+
+import jakarta.persistence.*;
+
+//JPA = Java Persistence API
+
+// Entity: transforma uma classe em uma entidade no BD
+@Entity
+// Table: designa a tabela especifica que vai se relacionar com o BD
+@Table(name = "tb_cadastro_Ninjas")
+public class NinjaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaEntity() {
+    }
+
+    public NinjaEntity(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
